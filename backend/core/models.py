d48434c1c5084e29e95b30654a71bbe965cfe59a
Kavya Ajo <kavyaajo222@gmail.com>
2026-06-06 18:35:18 +0530

    feat:setup Django project and database models

---
+++ backend/core/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here
+
+class User(models.Model):
+    username = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class AudioRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    audio_file = models.FileField(upload_to='audio/')
+    transcipt=models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class AIResult(models.Model):
+    audio=models.OneToOneField(AudioRecord,on_delete=models.CASCADE)
+    summary=models.TextField()
+    key_points=models.TextField()
+    translation=models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
