250065cd3383a2f08d3b28bd57b1970f421f102b
Kavya Ajo <kavyaajo222@gmail.com>
2026-06-13 23:27:44 +0530

    Add default databse values for CI

--- backend/voicebridge_backend/settings.py
+++ backend/voicebridge_backend/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'voicebridge_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
+        'NAME': env('DB_NAME', default='voicebridge_db'),
+        'USER': env('DB_USER', default='postgres'),
+        'PASSWORD': env('DB_PASSWORD', default='your-default-db-password'),
+        'HOST': env('DB_HOST', default='localhost'),
+        'PORT': env('DB_PORT', default='5432'),
     }
 }
 
