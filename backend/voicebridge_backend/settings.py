37ff528ce33476e345c2feade5c13b81d02b7cfd
Kavya Ajo <kavyaajo222@gmail.com>
2026-06-13 23:32:45 +0530

    Add default databse values for CI

--- backend/voicebridge_backend/settings.py
+++ backend/voicebridge_backend/settings.py
@@ -131,6 +131,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_Pagination_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
