6f7ae24af9294de4e36768232ddd0a4e896906b0
Kavya Ajo <kavyaajo222@gmail.com>
2026-06-17 14:27:37 +0530

    final commit

---
+++ backend/docs/index.md
@@ -0,0 +1,16 @@
+# VoiceBridge AI
+
+Welcome to the VoiceBridge AI documentation.
+
+## Features
+- User Authentication
+- Audio Upload
+- AI Transcription
+- AI Summarization
+- JWT Authentication
+
+## Tech Stack
+- Django
+- Django REST Framework
+- Supabase
+- Python
--- backend/voicebridge_backend/settings.py
+++ backend/voicebridge_backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'core',
     'rest_framework',
     'django_filters',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -133,4 +134,5 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
