6f3230e9eab5f5df81bbe7a66f6b59a6ee53ad72
Kavya Ajo <kavyaajo222@gmail.com>
2026-06-17 13:30:01 +0530

    Add API documentation, MkDocs, Swagger setup, and README updates

---
+++ backend/docs/index.md
@@ -0,0 +1,27 @@
+# VoiceBridge AI
+
+Welcome to the VoiceBridge AI documentation.
+
+## Features
+
+- User Authentication
+- Audio Upload
+- AI Transcript Generation
+- AI Summarization
+- Action Item Extraction
+- Swagger API Documentation
+
+## API Docs
+
+Visit:
+
+http://127.0.0.1:8000/api/schema/swagger-ui/
+
+## Tech Stack
+
+- Django
+- Django REST Framework
+- JWT
+- drf-spectacular
+- Supabase
+- Python
--- backend/voicebridge_backend/urls.py
+++ backend/voicebridge_backend/urls.py
@@ -23,6 +23,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
@@ -32,6 +33,8 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("api/ai/summary/", AISummaryView.as_view(), name="ai-summary"),
+   path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/swagger-ui/",SpectacularSwaggerView.as_view(url_name="schema"),name="swagger-ui",)
    ]
 
 
