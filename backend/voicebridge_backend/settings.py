18a869110f28d52c726f1adf9b9eb3074bd20955
Kavya Ajo <kavyaajo222@gmail.com>
2026-06-20 06:40:38 +0530

    final commit

--- backend/voicebridge_backend/settings.py
+++ backend/voicebridge_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY', default='your-default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['voicebridgeai-production.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
